0687982add37741c25ebe436f1c63cff66cfdd24
Salah Ashraf <65281113+salahashraf3@users.noreply.github.com>
2025-05-20 12:56:06 +0530

    import useEffect

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import style from "@/components/Navbar/Navbar.module.css";
 import logo from "@/assets/icons/icon.png";
 import menu from "@/assets/icons/menuIcon.svg";
